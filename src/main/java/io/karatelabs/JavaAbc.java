d93733db1f320a32e9ead1f963739ac9f38652cc
naresh <narasgonda777@gmail.com>
2024-10-12 18:33:11 +0530

    123|rahual|Java Abc added

---
+++ src/main/java/io/karatelabs/JavaAbc.java
@@ -0,0 +1,15 @@
+package io.karatelabs;
+
+public class JavaAbc {
+    public static void main(String[] args) {
+
+        int i=0;
+        int j=10;
+
+
+        while (i<j){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
